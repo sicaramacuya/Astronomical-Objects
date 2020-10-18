93ac555fbb6bea6be9341b8f30a322331a61fe09
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-10-18 17:52:59 -0400

    starting the "scripts.js" file

---
+++ resources/js/scripts.js
@@ -0,0 +1,31 @@
+import planets from './planets.js'
+
+console.log("Hello!")
+
+
+
+
+
+
+
+// const rightSectionContainer = document.getElementById('right-section-container')
+
+// // right section container and its childs
+// // the length of the planets.js determines how many times this loops goes around
+// for (let index=0; index < planets.lenght; ++index) {
+//     // creating a div element for the object name and appending it to the right section container
+//     let objectNameDiv = document.createElement('div');
+//     objectNameDiv.id = 'object-name'
+//     rightSectionContainer.appendChild(objectNameDiv)
+
+//     // creating h4 element for the name text and appending it to the objectNameDiv
+//     let nameTextDiv = document.createElement('h4')
+//     nameTextDiv.id = 'name-text'
+//     nameTextDiv.innerText = planets[index].name
+//     objectNameDiv.appendChild(nameTextDiv)
+
+//     console.log(planets[index].name)
+
+
+    
+// }
